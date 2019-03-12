530aa07c6960552228c6c3b0fd07bbc4ae5889d7
Michele Conway <mconway890@gmail.com>
2019-03-12 16:40:04 -0400

    Scrape imdb for quotes

---
+++ lib/thats_what_she_said/quote.rb
@@ -0,0 +1,22 @@
+class ThatsWhatSheSaid::Quote
+  attr_accessor :quote_text
+
+  def self.generate_quote
+    self.scrape_quotes
+  end
+
+  def self.scrape_quotes
+    quotes = []
+
+    quotes << self.scrape_1
+
+    quotes
+  end
+
+  def self.scrape_1
+    doc = Nokogiri::HTML(open("https://www.imdb.com/title/tt0664521/quotes?ref_=tt_ql_trv_4"))
+    quote = self.new
+    quote.quote_text = doc.search("div.sodatext").first.text
+    quote
+  end
+end
