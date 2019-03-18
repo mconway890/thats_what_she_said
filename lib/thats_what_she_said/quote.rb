f604a1d8696aef26f4ed1485dc79cd86c89dd7f9
Michele Conway <mconway890@gmail.com>
2019-03-18 07:54:26 -0400

    Use pry to check scrape return

--- lib/thats_what_she_said/quote.rb
+++ lib/thats_what_she_said/quote.rb
@@ -9,14 +9,14 @@ class ThatsWhatSheSaid::Quote
     quotes = []
 
     quotes << self.scrape_1
-
+    #binding.pry
     quotes
   end
 
   def self.scrape_1
     doc = Nokogiri::HTML(open("https://www.imdb.com/title/tt0664521/quotes?ref_=tt_ql_trv_4"))
     quote = self.new
-    quote.quote_text = doc.search("div.sodatext").text
+    quote.quote_text = doc.search("div.sodatext p").text
     quote
   end
 end
